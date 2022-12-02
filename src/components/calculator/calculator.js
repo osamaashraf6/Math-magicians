9d44ce402e3c1d3b5dc0b829c7e8526397f6b4e4
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-02 13:24:01 +0200

    Add the functionality to the calculator

--- src/components/calculator/calculator.js
+++ src/components/calculator/calculator.js
@@ -1,39 +1,65 @@
 import React, { Component } from 'react';
 import calculate from '../logic/calculate';
-import operate from '../logic/operate';
 import './calculator.css';
 
 // eslint-disable-next-line react/prefer-stateless-function
 export default class Calculator extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: '0',
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick = (e) => {
+    if (!e.target.name) return;
+    const { next, total, operation } = calculate(this.state, e.target.name);
+    if (next === null && total === null) {
+      this.setState({ next, total: '0', operation });
+    } else {
+      this.setState({ next, total, operation });
+    }
+  }
+
   render() {
+    const { next, total } = this.state;
+    const btns = [
+      'AC',
+      '+/-',
+      '%',
+      '÷',
+      '7',
+      '8',
+      '9',
+      '*',
+      '4',
+      '5',
+      '6',
+      '-',
+      '1',
+      '2',
+      '3',
+      '+',
+      '0',
+      '.',
+      '=',
+    ];
     return (
       <>
         {/* start sectiononee */}
         <section className="sectionone" id="sectionone">
           <div className="sectionone__wrapper">
             <div className="formcontrol">
-              <input type="text" name="name" placeholder="Enter the calc" />
+              {next ? (<input type="text" name="name" value={next} />) : (<input type="text" name="name" value={total} />)}
+
             </div>
             <div className="btns">
-              <button type="button">AC</button>
-              <button type="button">+/-</button>
-              <button type="button">%</button>
-              <button type="button">÷</button>
-              <button type="button">7</button>
-              <button type="button">8</button>
-              <button type="button">9</button>
-              <button type="button">*</button>
-              <button type="button">4</button>
-              <button type="button">5</button>
-              <button type="button">6</button>
-              <button type="button">-</button>
-              <button type="button">1</button>
-              <button type="button">2</button>
-              <button type="button">3</button>
-              <button type="button">+</button>
-              <button type="button">0</button>
-              <button type="button">.</button>
-              <button type="button">=</button>
+              {btns.map((btn) => (
+                <button type="button" name={btn} key={btn} onClick={(e) => this.handleClick(e)}>{btn}</button>
+              ))}
             </div>
           </div>
         </section>
