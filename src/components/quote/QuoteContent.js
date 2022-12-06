e9a075ccba4c3cac649366369be74bada27d32fd
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-06 18:30:22 +0200

    Add a class name for the div of quote

--- src/components/quote/QuoteContent.js
+++ src/components/quote/QuoteContent.js
@@ -3,7 +3,7 @@ import './QuoteContent.css';
 
 function QuoteContent() {
   return (
-    <div>
+    <div className="QuoteContent">
       Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, commodi maxime!
       Perferendis, doloremque vel velit rationevoluptas ducimus impedit ullam nihil
       enim. Possimus libero iusto ipsa placeat perspiciatis at quos?
