22b7e6819d290ff2c96ec8a60901bd6e1994c4a6
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-06 18:28:27 +0200

    Add class name for the welcome div

--- src/components/home/HomeContent.js
+++ src/components/home/HomeContent.js
@@ -4,7 +4,7 @@ import './HomeContent.css';
 function HomeContent() {
   return (
     <>
-      <div>Welcome To our Page</div>
+      <div className="welcome">Welcome To our Page</div>
       <p>
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem consectetur
         quisquam magni quo cumque soluta voluptatem corrupti totam quaerat perferendis
