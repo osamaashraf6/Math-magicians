96e70e7cd174b96329ee495d71b3eadcf575c807
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-06 15:26:57 +0200

    Add the pages folder for organize and navigate into the other pages of the app

---
+++ src/pages/Calculation.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Calculator from '../components/calculator/calculator';
+
+function Calculation() {
+  return (
+    <>
+      <div>Calculation</div>
+      <Calculator />
+    </>
+  );
+}
+
+export default Calculation;
---
+++ src/pages/Quote.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Quote() {
+  return (
+    <div>Quote</div>
+  );
+}
+
+export default Quote;
