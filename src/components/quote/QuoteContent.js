c334ba06294b476b169518b25fef92065b06d6c1
osamaashraf6 <osamaashraf668@gmail.com>
2022-12-06 15:45:23 +0200

    Add the HTML structure for the content of the Quote component and call the css file

---
+++ src/components/quote/QuoteContent.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './QuoteContent.css';
+
+function QuoteContent() {
+  return (
+    <div>
+      Lorem ipsum dolor, sit amet consectetur adipisicing elit. Placeat, commodi maxime!
+      Perferendis, doloremque vel velit rationevoluptas ducimus impedit ullam nihil
+      enim. Possimus libero iusto ipsa placeat perspiciatis at quos?
+    </div>
+  );
+}
+
+export default QuoteContent;
